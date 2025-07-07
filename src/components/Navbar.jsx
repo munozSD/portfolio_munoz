9eb0648337cbc11fa942cc6987b8578321cd9c0c
unknown <julioalb4488@gmail.com>
2025-07-06 22:57:06 -0600

    Redireccionamiento de urls en navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-import Contact from "./Contact";
+import { Link } from "react-router-dom"; // <-- Importa Link
+
 const Navbar = () => {
   return (
     <nav className="bg-gray-900 text-white p-4 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
-        <a href="../#" className="text-xl font-bold">
-          Julio Muñoz -->
-        </a>
+        <Link to="/" className="text-xl font-bold">
+          Julio Muñoz →
+        </Link>
         {/* Links */}
         <ul className="flex space-x-6">
           <li>
-            <a href="/" className="hover:text-gray-400">Inicio</a>
+            <Link to="/" className="hover:text-gray-400">Inicio</Link>
           </li>
           <li>
-            <a href="About" className="hover:text-gray-400">Sobre mí</a>
+            <Link to="/About" className="hover:text-gray-400">Sobre mí</Link>
           </li>
           <li>
-            <a href="Projects" className="hover:text-gray-400">Proyectos</a>
+            <Link to="/Projects" className="hover:text-gray-400">Proyectos</Link>
           </li>
           <li>
-            <a href="Contact" className="hover:text-gray-400">Contacto</a>
+            <Link to="/Contact" className="hover:text-gray-400">Contacto</Link>
           </li>
         </ul>
       </div>
